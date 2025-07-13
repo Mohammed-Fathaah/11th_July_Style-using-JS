35947858a99654c0fc71a7f09fbb6239983d9f67
mohammedfathaah <mohammedfathaah@gmail.com>
2025-07-13 23:03:36 +0530

    eighth question completed

---
+++ Eighth question/question8.js
@@ -0,0 +1,7 @@
+const allps=document.getElementsByTagName("p");
+for(let i=0;i<allps.length;i++){
+    allps[i].style.border="2px solid black";
+    allps[i].style.backgroundColor="skyblue";
+    allps[i].style.padding="10px";
+    allps[i].style.margin="10px";
+}
